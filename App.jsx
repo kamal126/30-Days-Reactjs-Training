d3a4cd4a2a3990555ee631b00deb2be788f0e146
Kamal Chandra <54989946+kamal126@users.noreply.github.com>
2024-10-14 13:32:23 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hey Kamal! Welcome Day-1-ReactJs-Training Session. </h1>
+    </div>
+    );
+}
+
+export default App;
